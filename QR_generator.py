0edc0ba013b4c61dae75239851eba02c4667da96
Shubh Bharadwaj <58884627+Shubh-Bharadwaj@users.noreply.github.com>
2023-09-24 21:03:16 +0530

    Update QR_generator.py

--- QR_generator.py
+++ QR_generator.py
@@ -3,7 +3,7 @@ from pyqrcode import QRCode
 print("Hello User!")
 print("This program helps you to generate QR code for any given URL.")
 print("="*40)
-s = input("Enter URL to generate QR Code: ")
+s = input("Enter URL to generate the QR Code: ")
 print("="*40)
 url = pyqrcode.create(s)
 url.svg("myqr.svg", scale = 8)
